6de5668544da9130f5e713f8c97e57bd6968ba9d
OlegMilkin <olegek.milkin@gmail.com>
2021-08-05 23:25:51 +0300

    todo add new container wrapper

--- src/TaskList/TaskListContainer.js
+++ src/TaskList/TaskListContainer.js
@@ -1,4 +1,3 @@
-import TaskList from "./TaskList";
 import {connect} from "react-redux";
 import {
   addNewTaskText,
@@ -11,6 +10,9 @@ import {
   changeCompletedStatus,
   setTasksDataAC,
 } from "../redux/task-list-reducer";
+import React from "react";
+import * as axios from "axios";
+import TaskList from "./TaskList";
 
 let mapStateToProps = (state) => {
   return {
@@ -52,5 +54,34 @@ let mapDispatchToProps = (dispatch) => {
   }
 };
 
-let TaskListContainer = connect(mapStateToProps, mapDispatchToProps)(TaskList)
+class TaskListAPI extends React.Component {
+
+  componentDidMount() {
+    axios.get('http://localhost:3000/tasksData')
+      .then(response => {
+        this.props.setTasksData(response.data)
+      })
+  }
+
+  render() {
+    return (
+      <TaskList
+        taskList={this.props.taskList}
+        removeTask={this.props.removeTask}
+        enableEditMode={this.props.enableEditMode}
+        disableEditMode={this.props.disableEditMode}
+        afterEditText={this.props.afterEditText}
+        changeEditTaskText={this.props.changeEditTaskText}
+        changeTaskTitle={this.props.changeTaskTitle}
+        changeCompletedStatus={this.props.changeCompletedStatus}
+        newTaskText={this.props.newTaskText}
+        addText={this.props.addText}
+        updateText={this.props.updateText}
+      />
+    )
+  }
+}
+
+
+let TaskListContainer = connect(mapStateToProps, mapDispatchToProps)(TaskListAPI)
 export default TaskListContainer;
