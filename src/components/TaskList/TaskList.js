f3ff98710f8b12a11bb3a22eca0b40197a0d595b
OlegMilkin <olegek.milkin@gmail.com>
2021-10-22 19:06:45 +0300

    data sort

--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -1,9 +1,8 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import {Redirect} from 'react-router-dom'
 import TaskItem from '../TaskItem/TaskItem'
 import NewTaskForm from '../NewTaskForm/NewTaskForm'
 import Loader from '../common/Loader/loader'
-
 
 const TaskList = (
   {
@@ -17,27 +16,49 @@ const TaskList = (
   }
 ) => {
 
-  let tasksEl = taskList.map((task) => {
-    return (
-      <TaskItem
-        taskItem={task}
-        key={task.id}
-        title={task.title}
-        isTaskCompleted={task.completed}
-        removeThunk={removeThunk}
-        updateStatusThunk={updateStatusThunk}
-        changeTaskTitleThunk={changeTaskTitleThunk}
-      />
-    )
-  })
+  let [tasks, setTasks] = useState(taskList)
+  let [searchString, setSearchString] = useState('')
+
+  useEffect(() => {
+    setTasks(taskList)
+  }, [taskList])
 
   const sortHighToLow = () => {
-    console.log(taskList.sort((a, b) => a.endData > b.endData ? 1 : -1))
+    let sortedTasks = [...tasks].sort((a, b) => a.endData < b.endData ? 1 : -1)
+    setTasks(sortedTasks)
   }
 
   const sortLowToHigh = () => {
-    console.log(taskList.sort((a, b) => a.endData < b.endData ? 1 : -1))
+    let sortedTasks = [...tasks].sort((a, b) => a.endData > b.endData ? 1 : -1)
+    setTasks(sortedTasks)
   }
+
+  const filterCompleted = () => {
+    let filteredTasks = tasks.filter((task) => {return task.completed})
+    setTasks(filteredTasks)
+  }
+
+  const resetFilters = () => {
+    setTasks(taskList)
+    setSearchString('')
+  }
+
+  const onSearchChange = () => {
+    let searchValue = searchInput.current.value
+    setSearchString(searchValue)
+    sortIncludedString(searchValue)
+  }
+
+  const sortIncludedString = (value) => {
+
+    let filteredTasks = tasks.filter((task) => {
+      return task.title.toLowerCase().includes(value.toLowerCase())
+    })
+
+    setTasks(filteredTasks)
+  }
+
+  let searchInput = React.createRef();
 
   if (!isLogged) {
     return <Redirect to="/auth"/>
@@ -58,7 +79,19 @@ const TaskList = (
   return (
     <div className="container">
       <div className="row">
+        <div className="col-lg-12 mb-3">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Find some task"
+            value={ searchString }
+            onChange={ onSearchChange }
+            ref={ searchInput }
+          />
+        </div>
         <div className="col-lg-12">
+          <button className="btn btn-success" onClick={ filterCompleted }>Completed</button>&nbsp;
+          <button className="btn btn-success" onClick={ resetFilters }>Reset</button>
           <div className="dropdown mb-3 float-end">
             <button
               className="btn btn-secondary dropdown-toggle btn-sm"
@@ -95,9 +128,22 @@ const TaskList = (
         <div className="col-lg-12">
           <div className="list-group">
             {
-              tasksEl.length > 0
+              tasks.length > 0
                 ?
-                tasksEl
+
+                tasks.map((task) => {
+                  return (
+                    <TaskItem
+                      taskItem={task}
+                      key={task.id}
+                      title={task.title}
+                      isTaskCompleted={task.completed}
+                      removeThunk={removeThunk}
+                      updateStatusThunk={updateStatusThunk}
+                      changeTaskTitleThunk={changeTaskTitleThunk}
+                    />
+                  )
+                })
                 :
                 <div className="mt-5 alert alert-secondary text-center">
                   You don't have any tasks
